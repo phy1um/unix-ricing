92d7c6d591f43163a6a536cbab2cc8892f7447c9
Tom Marks <tom@tommarks.xyz>
2021-07-25 23:24:22 +1000

    DWM Cleanup colour variables

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,10 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-#define COL_PRIMARY "#2EB398"
-#define COL_2RY "#167562"
-#define COL_WHITE "#eeeeee"
-#define COL_BG "#2C2E33"
-
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -12,12 +7,12 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Roboto Condensed:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const char col_primary[]       = "#2eb398";
+static const char col_2ry[]       = "#167562";
+static const char col_fg[]       = "#eeeeee";
+static const char col_bg[]       = "#2c2e33";
+
+static const unsigned int baralpha = 0x10U;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -68,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *launchercmd[] = {"rofi", "-show", "run", NULL};
 
