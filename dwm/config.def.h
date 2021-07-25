fab426b4cf7be4638109602e2660b20e785365b6
Tom Marks <tom@tommarks.xyz>
2021-07-25 23:40:49 +1000

    DWM bar padding patch

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,19 +5,21 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Roboto Condensed:size=10" };
+static const char *fonts[]          = { "Roboto:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_primary[]       = "#2eb398";
 static const char col_2ry[]       = "#167562";
 static const char col_fg[]       = "#eeeeee";
 static const char col_bg[]       = "#2c2e33";
+static const int vertpad = 10;
+static const int sidepad = 10;
 
 static const unsigned int baralpha = 0x10U;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { COL_WHITE , COL_BG, COL_2RY},
-	[SchemeSel]  = { COL_WHITE , COL_BG,  COL_PRIMARY},
+	[SchemeNorm] = { col_fg, col_bg, col_2ry},
+	[SchemeSel]  = { col_primary, col_bg,  col_primary},
 };
 
 static const unsigned int alpha[][3] 	= {
@@ -62,7 +64,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+// static char dmenumon[2] = "0"; 
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *launchercmd[] = {"rofi", "-show", "run", NULL};
