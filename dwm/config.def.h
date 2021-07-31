64bb69cf4b9b33a18a7e0480b6eba95f911562ef
Tom Marks <tom@tommarks.xyz>
2021-07-31 22:57:41 +1000

    DWM Config PCSX2 and Xephyr to float

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -38,6 +38,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+        { "PCSX2",    NULL,       NULL,       1 << 7,       1,           -1 },
+        { "Xephyr",   NULL,       NULL,       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
